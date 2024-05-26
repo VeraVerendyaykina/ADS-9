69d66034d3b0a6d9287407de5f16dac3801e9796
VeraVerendyaykina <151723475+VeraVerendyaykina@users.noreply.github.com>
2024-05-26 21:22:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,20 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
-
-BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<std::string> makeTree(const char *filename) {
+    std::ifstream file(filename);
+    BST<std::string> tree;
+    std::string word;
+    while (!file.eof()) {
+        char ch = file.get();
+        if (ch != ' ' && isalpha(ch)) {
+            ch = tolower(ch);
+            word += ch;
+        } else {
+            tree.insert(word);
+            word.clear();
+        }
+    }
+    return tree;
 }
