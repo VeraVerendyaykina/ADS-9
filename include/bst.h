bf3e01e6a86213d7c52c5f0f4a8f04c54d3ddc69
VeraVerendyaykina <151723475+VeraVerendyaykina@users.noreply.github.com>
2024-05-26 21:19:35 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,81 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include <algorithm>
+#include <string>
 
+template<typename T>
+struct Node {
+    T key;
+    int count;
+    Node *left, *right;
+
+    explicit Node(T k) : key(k), count(1), left(nullptr), right(nullptr) {}
+};
+
+template<typename T>
+class BST {
+ private:
+    Node<T> *root;
+
+    int Height(Node<T> *p) {
+        if (p == nullptr)
+            return 0;
+        int hr = Height(p->right);
+        int hl = Height(p->left);
+        return std::max(hr, hl) + 1;
+    }
+
+    Node<T> *insert(Node<T> *p, T k) {
+        if (p == nullptr) {
+            p = new Node<T>(k);
+        } else if (p->key > k) {
+            p->left = insert(p->left, k);
+        } else if (p->key < k) {
+            p->right = insert(p->right, k);
+        } else {
+            p->count++;
+        }
+        return p;
+    }
+
+    int findVal(Node<T> *p, T k) {
+        if (p->key == k)
+            return p->count;
+        else if (p->key > k)
+            return findVal(p->left, k);
+        else if (p->key < k)
+            return findVal(p->right, k);
+        else
+            return 0;
+    }
+
+    void deleteTree(Node<T> *p) {
+        if (p == nullptr)
+            return;
+        deleteTree(p->right);
+        deleteTree(p->left);
+        delete p;
+        p = nullptr;
+    }
+
+ public:
+    BST() : root(nullptr) {}
+
+    void insert(T k) {
+        root = insert(root, k);
+    }
+
+    int depth() {
+        return Height(root) - 1;
+    }
+
+    int search(T k) {
+        return findVal(root, k);
+    }
+
+    ~BST() {
+        deleteTree(root);
+    }
+};
 #endif  // INCLUDE_BST_H_
